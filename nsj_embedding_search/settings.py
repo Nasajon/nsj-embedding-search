d3cb9979b34aa297bf417f51111f59ecaf10eb4c
Sergio Silva <sergiosilva@nasajon.com.br>
2025-05-22 21:12:37 -0300

    Início dos testes

--- nsj_embedding_search/settings.py
+++ nsj_embedding_search/settings.py
@@ -7,8 +7,8 @@ OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 OPENAI_EMBEDDING_MODEL = os.getenv("OPENAI_EMBEDDING_MODEL", "text-embedding-3-large")
 OPENAI_EMBEDDING_MODEL_LIMIT = int(os.getenv("OPENAI_EMBEDDING_MODEL_LIMIT", "8191"))
 
-CHUNCK_SIZE = int(os.getenv("OPENAI_API_KEY"), 256)
-OVERLAP_SIZE = int(os.getenv("OVERLAP_SIZE"), 128)
+CHUNCK_SIZE = int(os.getenv("CHUNCK_SIZE", "256"))
+OVERLAP_SIZE = int(os.getenv("OVERLAP_SIZE", "128"))
 
 # Configurando o logger
 logger = logging.getLogger(APP_NAME)
